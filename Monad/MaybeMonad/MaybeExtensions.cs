139edffc3ac5cac5fba85d4cf616c3b80210637d
Rex Core <desire.mov@gmail.com>
2015-10-24 23:18:12 +0300

    Added basic ToMaybe extension

---
+++ Monad/MaybeMonad/MaybeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monad.MaybeMonad
+{
+    public static class MaybeExtensions
+    {
+        /// <summary>
+        /// Converts value to an Maybe<T>.
+        /// </summary>
+        public static Maybe<T> ToMaybe<T>(this T self)
+        {
+            return self == null
+                    ? Nothing<T>.Default
+                    : Some<T>.Of(self);
+        }
+    }
+}
